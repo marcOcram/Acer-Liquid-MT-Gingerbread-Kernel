ae9f0bb9a85ddefb5eec1fad51519474d10fc466
marcOcram <O_C_R_A_M@gmx.de>
2011-10-28 17:14:47 +0200

    added interactiveX governor

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -375,6 +375,9 @@ extern struct cpufreq_governor cpufreq_gov_lagfree;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOOTHASS)
 extern struct cpufreq_governor cpufreq_gov_smoothass;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smoothass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_interactivex)
 #endif
 
 
