c740611afcacf8e3a24c5ce338144f9ad184046d
marcOcram <O_C_R_A_M@gmx.de>
2011-11-03 17:54:44 +0100

    add smartassV2 governor

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -381,6 +381,9 @@ extern struct cpufreq_governor cpufreq_gov_interactivex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MINMAX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_minmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
 #endif
 
 
