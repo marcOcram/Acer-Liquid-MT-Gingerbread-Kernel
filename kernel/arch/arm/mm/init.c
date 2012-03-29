eb792ca3fe4b97568bbe2f8f98cbe11fe485ef4f
marcOcram <O_C_R_A_M@gmx.de>
2012-03-29 15:06:38 +0200

    update source to M7630AABBQMLZA404030

--- kernel/arch/arm/mm/init.c
+++ kernel/arch/arm/mm/init.c
@@ -547,7 +547,8 @@ static void __init free_unused_memmap_node(int node, struct meminfo *mi)
 	for_each_nodebank(i, mi, node) {
 		struct membank *bank = &mi->bank[i];
 
-		bank_start = bank_pfn_start(bank);
+		bank_start = round_down(bank_pfn_start(bank),
+					MAX_ORDER_NR_PAGES);
 
 		/*
 		 * If we had a previous bank, and there is a space
@@ -556,12 +557,8 @@ static void __init free_unused_memmap_node(int node, struct meminfo *mi)
 		if (prev_bank_end && prev_bank_end < bank_start)
 			free_memmap(node, prev_bank_end, bank_start);
 
-		/*
-		 * Align up here since the VM subsystem insists that the
-		 * memmap entries are valid from the bank end aligned to
-		 * MAX_ORDER_NR_PAGES.
-		 */
-		prev_bank_end = ALIGN(bank_pfn_end(bank), MAX_ORDER_NR_PAGES);
+		prev_bank_end = round_up(bank_pfn_end(bank),
+					 MAX_ORDER_NR_PAGES);
 	}
 }
 
